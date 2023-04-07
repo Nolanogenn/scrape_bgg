4279ccb359f8bd6b80f18ad114473f765926d930
Gennaro Nolano <nolanogenn@gmail.com>
2023-04-07 08:45:31 +0200

    initial commit

---
+++ get_top100.py
@@ -0,0 +1,75 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import date
+from scrape_info_games import scrape_info_game
+import json
+from tqdm import tqdm
+
+class scrape_bgg:
+    def __init__(self):
+        url = "https://boardgamegeek.com/browse/boardgame"
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        table = soup.find('table', attrs = {"class" : "collection_table"})
+        self.today = date.today()
+        self.rows = table.find_all('tr')
+        self.columns = [
+                        'standing', 
+                        'title',
+                        'year',
+                        'description', 
+                        'geek_rating',
+                        'avg_rating',
+                        'num_voters',
+                        'url'
+                        ]
+    
+    def clean_title(self, string_title):
+        string_title = string_title.split('\n')
+        title = string_title[0]
+        year = int(string_title[1][1:-1])
+        description = string_title[4].replace('\t', '')
+        return title, year, description
+
+    def get_df_and_json(self):
+        df = []
+        json_file = []
+
+        for row in tqdm(self.rows):
+            cols = row.find_all('td')
+            cols_url = [ele for ele in cols]
+            cols_elem = [ele.text.strip() for ele in cols]
+            cols_elem = [ele for ele in cols_elem if cols][:-1]
+            if len(cols_elem) > 0:
+
+                url_tailing = cols_url[2].find_all('a', href=True)[0]['href']
+                url = "https://boardgamegeek.com/{}".format(url_tailing)
+            
+                place = int(cols_elem[0])
+                title, year, description = self.clean_title(cols_elem[2])
+                geek_rating = float(cols_elem[3])
+                avg_rating = float(cols_elem[4])
+                num_voters = int(cols_elem[5])
+                
+                row_clean = [place,title, year, description, geek_rating, avg_rating, num_voters, url]
+                df.append(row_clean)
+                
+                dict_game = scrape_info_game(url)
+                dict_game = dict_game.get_info()
+
+                json_file.append(dict_game)
+
+        df = pd.DataFrame(df, columns = self.columns)
+        filename = './files/bgg_scrape_top100_{}'.format(self.today)
+        
+
+        df.to_csv(f"{filename}.csv")
+        with open(f"./files/top_100_latest.json", 'w+') as f:
+            json.dump(json_file, f)
+
+if __name__ == '__main__':
+    print("scraping the top 100 from bgg...")
+    ws = scrape_bgg()
+    ws.get_df_and_json()
+    print("done!")
---
+++ scrape_info_games.py
@@ -0,0 +1,73 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import pprint
+import json
+import re
+
+CLEANR = re.compile('<.*?>') 
+
+
+class scrape_info_game:
+    def __init__(self, url):
+        page = requests.get(url)
+        self.soup = BeautifulSoup(page.content, 'html.parser')
+        self.url = url
+    def get_info(self):
+        info = self.soup.find_all("script")
+        script = info[2]
+        jsonStr = script.text.strip()
+        jsonStr = jsonStr.split(';\n')
+        jsonStr = jsonStr[9].strip()
+        info = json.loads(jsonStr[23:])
+
+        json_game = {
+                "url" : self.url,
+                "id" : info['item']['id'],
+                "name" : info['item']['name'],
+                "images" : info['item']['images'],
+                "website" : info['item']['website'],
+                "description" : clean_html(info['item']['description']),
+                "short_description": info['item']['short_description'],
+                "yearpublished" : info['item']['yearpublished'],
+                "minplayers" : info['item']['minplayers'],
+                "maxplayers" : info['item']['maxplayers'],
+                "minplaytime" : info['item']['minplaytime'],
+                "maxplaytime" : info['item']['maxplaytime'],
+                "minage" : info['item']['minage'],
+                "stats" : info['item']['stats'],
+                "polls" : info['item']['polls'],
+                "accessories" : info['item']['links']['boardgameaccessory'],
+                "artist" : info['item']['links']['boardgameartist'],
+                "category": info['item']['links']['boardgamecategory'],
+                "designer" : info['item']['links']['boardgamedesigner'],
+                "developer" : info['item']['links']['boardgamedeveloper'],
+                "editor" : info['item']['links']['boardgameeditor'],
+                "graphic_designer" : info['item']['links']['boardgamegraphicdesigner'],
+                "honor" : info['item']['links']['boardgamehonor'],
+                "mechanic" : info['item']['links']['boardgamemechanic'],
+                "publisher" : info['item']['links']['boardgamepublisher'],
+                "sculptor" : info['item']['links']['boardgamesculptor'],
+                "solodesigner" : info['item']['links']['boardgamesolodesigner'],
+                "subdomain" : info['item']['links']['boardgamesubdomain'],
+                "writer" : info['item']['links']['boardgamewriter'],
+                "containedin" : info['item']['links']['containedin'],
+                "contains" : info['item']['links']['contains'],
+                "expands" : info['item']['links']['expandsboardgame'],
+                "reimplementation" : info['item']['links']['reimplementation'],
+                "reimplements" : info['item']['links']['reimplements'],
+                "videogamebg" : info['item']['links']['videogamebg']
+                }    
+
+        return json_game
+
+def clean_html(html):
+    cleantext = re.sub(CLEANR, '', html)
+    return cleantext
+
+if __name__ == '__main__':
+
+    info = scrape_info_game("https://boardgamegeek.com/boardgame/174430/gloomhaven")
+    info = info.get_info()
+    
+
